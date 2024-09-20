9e849d3121bdfb58d1660058e675a8f49deb81b4
Abdul <>
2024-09-20 17:54:08 +0530

    first commit

---
+++ src/Components/Home.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+const Home = () => {
+    const abdul={
+       color:"white",
+    }
+  return (
+    <div>
+      <div className='container'>
+      <div className='container row justify-content-center '>
+        <h1 style={abdul} className=" text-center bg-danger col-12">CRM Software</h1>
+        </div>
+      </div>
+    </div>
+  )
+}
+export default Home
+
+
